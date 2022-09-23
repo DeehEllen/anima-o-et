c4e72f3f6f5f97036aa45f0853ef913fe1eb0575
Déborah Ellen <99999948+DeehEllen@users.noreply.github.com>
2022-09-22 22:20:33 -0300

    enviando arquidos

---
+++ script.js
@@ -0,0 +1,8 @@
+
+		
+        function startAnimation() {
+            document.getElementById("animacao").style.animationPlayState="running"
+        }
+        function stopAnimation() {
+            document.getElementById("animacao").style.animationPlayState="paused"
+        }
